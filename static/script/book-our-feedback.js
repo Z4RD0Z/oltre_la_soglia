87a08bfba5bc1adf3b82242faa22b09c428f15b4
Andrea Lomazzo <58508926+Z4RD0Z@users.noreply.github.com>
2026-08-12 16:28:11 +0000

    Refactor style for feedback; feat: add redirect for 404; feat: add better message for fetch; style: update feedback style;

--- static/script/book-our-feedback.js
+++ static/script/book-our-feedback.js
@@ -3,10 +3,21 @@ const bookID = params.get('id');
 const bookName = params.get('name');
 
 fetch(`/static/documents/${bookID}/index.md`)
-    .then(r => r.text()).then(md => {
-        document.getElementById('title').innerHTML = `<h2>I nostri pareri su: ${bookName}</h2>`;
+    .then(r => {
+        if (!r.ok) throw new Error(r.status);
+        return r.text();
+    })
+    .then(md => {
+        document.getElementById('title').innerHTML =
+            `<h2>I nostri pareri su: ${bookName}</h2>`;
         document.getElementById('content').innerHTML = marked.parse(md);
-    }).catch(err => {
-        console.error(err);
-        window.location.href = '/404.html';
+    })
+    .catch(err => {
+        console.log(err);
+        if (err.message === '404') {
+            document.getElementById('content').innerHTML = '<p>Non esistono ancora pareri per il libro selezionato.</p>';
+        } else {
+            document.getElementById('content').innerHTML = '<p>Si è verificato un errore nel caricamento del contenuto. Riprova più tardi.</p>';
+        }
     });
+
